e8154b8e6cd96f0d9ba14711bb3afd7a9cde4004
AchcarLucas <achcarlucas@hotmail.com>
2025-04-26 16:28:07 -0300

    - healthcheck root

--- base_project/public_site/views.py
+++ base_project/public_site/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import Product
 
 # Create your views here.
+
+# healthcheck
+def health(request):
+    return JsonResponse({'health': f'alive'})
+
+# home page
 def home(request):
     supply = Product.objects.filter(load_type=dict(Product.CONSTANT_LOAD_TYPE).get('supply'), avaliable=True)
     source = Product.objects.filter(load_type=dict(Product.CONSTANT_LOAD_TYPE).get('source'), avaliable=True)
